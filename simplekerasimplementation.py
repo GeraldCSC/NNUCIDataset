b0d0c1c45aa89077643c6baf69b85ab6aaff007b
Gerald Shen <gerald.shen@mail.utoronto.ca>
2020-01-03 21:19:29 -0500

    Removing extra files

--- simplekerasimplementation.py
+++
@@ -1,9 +0,0 @@
-import keras
-from keras import Sequential
-from keras import Dense
-import numpy as np
-
-train_data = np.load("normalized_train_data.npy")
-train_label = np.load("train_label.npy")
-test_data = np.load("normalized_test_data.npy")
-test_label = np.load("test_label.npy")
